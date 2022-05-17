567c8fa97ea2e5b5d08b96344dca573cc026d2c2
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2022-05-16 22:47:36 -0500

    agregada api de fechas proyecto

--- src/Controllers/ProjectController.php
+++ src/Controllers/ProjectController.php
@@ -11,6 +11,9 @@ use App\Services\Project\UploadRegisterForm;
 use App\Services\Project\GetProjectInfo;
 use App\Services\Project\UploadImage;
 
+use App\App\Constants;
+use App\App\Database;
+
 class ProjectController
 {
     public function createOneAuthor(Request $request, Response $response): Response
@@ -60,6 +63,46 @@ class ProjectController
         $params['project_image'] = $files['project_image'] ?? '';
         $uploadImage = new UploadImage($params);
         $response->getBody()->write(json_encode($uploadImage()));
+        return $response;
+    }
+
+    public function fechas(Request $request, Response $response): Response
+    {
+        $params = (array)$request->getParsedBody();
+        $fecha_inicio = $params['fecha_inicio'];
+        $fecha_fin = $params['fecha_fin'];
+        try{
+            $sql = 'SELECT COUNT(*) as total FROM fechas_proyectos';
+            
+            $db = new Database();
+            $db = $db->connect();
+            $result = $db->query($sql);
+            $count = $result->fetch()['total'];
+            if($count == "0") {
+                $sql = 'INSERT INTO fechas_proyectos (fecha_inicio, fecha_fin)
+                        VALUES  (:fecha_inicio, :fecha_fin);';
+                $result = $db->prepare($sql);
+                $result->bindParam(':fecha_inicio', $fecha_inicio);
+                $result->bindParam(':fecha_fin', $fecha_fin);
+                $result->execute();
+                $response->getBody()->write(json_encode(true));
+            } else {
+                $sql = 'SELECT * FROM fechas_proyectos';
+                $result = $db->query($sql);
+                $id_fechas_proyectos = $result->fetch()['id_fechas_proyectos'];
+                $sql = 'UPDATE fechas_proyectos
+                        SET  fecha_inicio = :fecha_inicio, fecha_fin = :fecha_fin;
+                        WHERE id_fechas_proyectos = :id_fechas_proyectos';
+                $result = $db->prepare($sql);
+                $result->bindParam(':fecha_inicio', $fecha_inicio);
+                $result->bindParam(':fecha_fin', $fecha_fin);
+                $result->bindParam(':id_fechas_proyectos', $id_fechas_proyectos);
+                $result->execute();
+                $response->getBody()->write(json_encode(true));
+            }
+        } catch(\Exception $exception) {
+            echo '{"error": ' . $exception->getMessage() . '}';
+        }
         return $response;
     }
 }
